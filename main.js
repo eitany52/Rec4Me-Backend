431070d8922e2f01ea66c47bf3a154747d0e6de3
Eran Bolandian <eranbolan91@gmail.com>
2022-01-10 20:16:46 +0200

    added router and succseed pass data

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
+const router = express.Router();
 const app = express();
 
-app.post('/login',(req,res)=>{
-   // console.log(req);
-    console.log(req.params);
-    console.log(res.params);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/",router);
+
+router.post('/login',(req,res)=>{
+    console.log(req.body);
 })
 
 app.post('/register',(req,res)=>{
     console.log(req);
     console.log(res);
-    
 })
 
 app.get('/',(req,res)=>{
