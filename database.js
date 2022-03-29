f8b299f7ea8838326e6500ae00f81054237767ef
Eran Bolandian <eranbolan91@gmail.com>
2022-03-30 00:27:39 +0300

    working on jwt auth

--- database.js
+++ database.js
@@ -1,42 +1,54 @@
-const { Client } = require('pg');
 const bcryptjs = require('bcryptjs');
-//const { password } = require('pg/lib/defaults');
+const {Pool} = require('pg')
 
-const client = new Client({
+const pool = new Pool({
   user: 'fs-info',
   host: '192.114.5.161',
   database: 'fs-info-db',
   password: '123',
   port: 5432,
+  "max": 20, // Max 20 connection
+  "connectionTimeoutMillis" : 0, // if all the connection are busy, "0" means "wait forever" (we can change it to shorter time)
+  "idleTimeoutMillis": 0, // this destroy the connection if it is no use. "0" means never - but we can set any other number and it will be in seconds
 })
-client.connect();
 
 
 // Login method. Gets company id and password, then connect to the database
 // and checks if they exists
 const login = function(companyID, password){
-    client.query("SELECT company_id, password FROM users_info WHERE company_id="
-            + companyID + "AND password=" + password,[1],(err,res)=>{
-                if(!err){
-                    console.log(res.rows);
-                }else{
-                    console.log(err);
-                }
-            client.end();
+    return new Promise(function(resolve, reject){
+        if(companyID === "" || password === ""){
+            reject(-1);
+        }
+        pool.query(`SELECT company_id, password FROM users_info WHERE company_id= 
+        ${companyID} AND password= '${password}'`).then(result =>{
+            resolve(1);
+        }).catch(err =>{
+            reject(-1);
+        }).then(()=> {});
+    })
+}
+
+// This func return a promise. IF register was succsessful, it will return 1, else - it will return -1
+const register = function(companyID, password, email, name, establishment, domain, occupation, location, size, amountManagers,
+                         amountEmployees, amountCEO, systemUsed){
+        return new Promise(function(resolve, reject){
+
+            // This if checks if these variables are numbers. IF they are not numbers -> reject!
+            if(isNaN(companyID) || isNaN(occupation) || isNaN(size) || isNan(amountManagers) || isNaN(amountEmployees) || isNaN(amountCEO)){
+                reject(-1)
+            }
+            pool.query(`INSERT INTO users_info (company_id, year_establishment, organization_size, num_of_ceo, num_of_managers, num_of_employees, email, organization_domain, organization_name, occupation, location, organization_system_used, password)
+                VALUES( ${companyID} , ${establishment}, ${size} , ${amountCEO} ,${amountManagers},${amountEmployees}, '${email}', '${domain}' , '${name}', '${occupation}' , '${location}' , '${systemUsed}', '${password}' )`).
+                then(result => {
+                    resolve(1);
+                }).catch(e => {
+                    console.error(e.stack)
+                    reject(-1);
+                }).then(() => {
+                });
         });
 }
 
-
-const register = function(companyID, password, email, name, establishment, domain, occupation, location, size, amountManagers,
-                         amountEmployees, amountCEO, systemUsed){
-        client.query(`INSERT INTO users_info (company_id, year_establishment, organization_size, num_of_ceo, num_of_managers, num_of_employees, email, organization_domain, organization_name, occupation, location, organization_system_used, password) VALUES( ${companyID} , ${establishment}, ${size} , ${amountCEO} ,${amountManagers},${amountEmployees}, '${email}', '${domain}' , '${name}', '${occupation}' , '${location}' , '${systemUsed}', '${password}' )` ,(err,res)=>{
-                        if(!err){
-                            console.log(res.rows);
-                        }else{
-                            console.log(err);
-                        }
-               // client.end();
-            });
-}
 
 module.exports = {register, login};
--- main.js
+++ main.js
@@ -1,41 +1,70 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const database = require('./database');
 const app = express();
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/",router);
 
 
-router.post('/login',(req,res)=>{
-    console.log(req.body);
-    const id = req.body.companyID;
-    const pass = req.body.companyPassword;
-    console.log(id);
-    database.login();
-})
+router.post('/login', (req,res)=>{
+    database.login(req.body.companyID, req.body.companyPassword).then(data =>{
+        console.log(data);
+        //res.redirect('admin_panel.html');
+        //res.json({accessToken: accessToken});
+        
+        const user = { companyID: req.body.companyID};
+        // creating access token with the user details
+        const accessToken = {token: jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)} ;
+        res.render('/adminpanel',accessToken);
+        
+    }).catch(err =>{
+        res.send()
+        console.log(err);
+    });
+});
 
 app.post('/register', (req,res)=>{
-    console.log(req.body);
-    database.register(req.body.companyID,req.body.companyPassword,req.body.companyEmail,
-        req.body.organizationName,req.body.dateOfEstablishment,req.body.organizationDomain,
-        req.body.companyOccupation, req.body.companyLocation, req.body.organizationSize, req.body.companyAmountOfManagers, req.body.companyAmountOfEmployees,
-        req.body.companyNumOfCeo, req.body.organizationSystemUsed);
-    
-})
+    database.register(req.body.companyID,req.body.companyPassword,req.body.email,
+        req.body.compName,req.body.establishment,req.body.domain,
+        req.body.occupation, req.body.location, req.body.size, req.body.numOfManagers, req.body.numOfEmployees,
+        req.body.numOfCeo, req.body.systemUsed).then((data) =>{
+            console.log(data);
+            res.send(1);
+        }).catch(err => {
+            console.log(err);
+            res.send(-1);
+        });
+});
 
-app.get('/',(req,res)=>{
-    res.redirect('/login');
-})
+app.get('/adminpanel',authenticateToken,(req,res)=>{
+
+});
 
 app.get('/home',(req,res)=>{
     res.redirect('/home');
 })
 
+// middleware function that check if the token is valid
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401); // 401 - means, user did not send token to the server
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, user)=> {
+        if(err) return res.sendStatus(403) // 403 - means token is no longer valid - so user have no access
+        req.user = user;
+        next;
+    })
+}
+
 const port = 3000
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
